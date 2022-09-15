f4269bf609e1b598ddf3564a56b1e368ac89975a
Boedoetmom <trimom806@gmail.com>
2022-09-15 19:36:41 +0700

    creating seeder & fixing error

--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+       $this->call ([
+
+            TypeUserSeeder::class,
+            ConsultationSeeder::class,
+            ConfigPaymentSeeder::class,
+            SpecialistSeeder::class,
+
+       ]);
     }
 }
